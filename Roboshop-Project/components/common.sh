e5f9c63b3e71ab7e15816f350fd49cede8d4c4fd
Charan <rpasyanthi@gmail.com>
2021-04-15 00:06:22 +0530

    16

--- Roboshop-Project/components/common.sh
+++ Roboshop-Project/components/common.sh
@@ -1,3 +1,17 @@
+USER_ID=$(id -u)
+case $USER_ID in
+ 0)
+   true
+   ;;
+ *)
+   echo "You need to be login as root to perform this operation"
+   exit 1
+   ;;
+ esac
+
+
+
+
 INFO(){
   echo -e "\e[1;32m[INFO] \e[1;34m[$COMPONENT] \e[1;33m[$(date '+%F %T')]\e[0m $1"
 }
--- Roboshop-Project/components/frontend.sh
+++ Roboshop-Project/components/frontend.sh
@@ -6,5 +6,6 @@ source ./components/common.sh
 
 INFO "Setup frontend started"
 INFO "Installing Nginx"
+yum install nginx -y
 SUCC "Installed Frontend"
 FAIL "Installing Frontend failed"
