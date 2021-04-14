d1988fb06851f096932b611fc6a427679c21cd2f
Charan <rpasyanthi@gmail.com>
2021-04-14 23:06:55 +0530

    9

--- Roboshop-Project/components/mongo.sh
+++ Roboshop-Project/components/mongo.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 #COMPONENT=Frontend
-source  E:\Repos\ShellScript\Roboshop-Project\components/common.sh
+
+source  E:\Repos\ShellScript\Roboshop-Project\components\common.sh
 
 
 INFO "Setup mongo started"
