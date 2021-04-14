6dd3692f41d987d1bd155a760097ac4b8873cfe7
Charan <rpasyanthi@gmail.com>
2021-04-14 22:23:44 +0530

    Project FE3

--- Roboshop-Project/components/frontend.sh
+++ Roboshop-Project/components/frontend.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo -e "\e[1;32m[INFO] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')] Installing Frontend"
+echo -e "\e[1;32m[INFO] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')] Installing Frontend\e[0m"
