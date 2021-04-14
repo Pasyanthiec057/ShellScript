bd638729fce1d3c857b754612b4b65c9f9a21be8
Charan <rpasyanthi@gmail.com>
2021-04-14 22:29:19 +0530

    Project FE3

--- Roboshop-Project/components/frontend.sh
+++ Roboshop-Project/components/frontend.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-echo -e "\e[1;32m[INFO] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')]\e[0m Installing Frontend"
+echo -e "\e[1;32m[INFO] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')]\e[0m Setup Frontend started"
+echo -e "\e[1;32m[INFO] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')]\e[0m Installing Nginx"
+echo -e "\e[1;32m[SUCC] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')]\e[0m Installed Frontend"
+echo -e "\e[1;31m[FAIL] \e[1;34m[FRONTEND] \e[1;33m[$(date '+%F %T')]\e[0m Installing Frontend failed"
