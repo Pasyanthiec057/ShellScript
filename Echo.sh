12c5e823c82913dec53380ace457101fc3c712b2
Charan <rpasyanthi@gmail.com>
2021-04-14 17:34:41 +0530

    Echo 11 push

--- Echo.sh
+++ Echo.sh
@@ -3,4 +3,4 @@
 echo  -e "hi\n\nthis is hello \nworld"
 echo 'Good\nmorning'
 echo -e "\e[41;32mGREEN ON RED\e[0m"
-echo -e "\e[1;43;32mGREEN ON YELLOW BOLD\e[0m"
+echo -e "\e[4;43;32mGREEN ON YELLOW BOLD\e[0m"
