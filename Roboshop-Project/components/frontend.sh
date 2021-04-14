1549c3ded84c9c94b628acce8ce3c6b82228f26f
Charan <rpasyanthi@gmail.com>
2021-04-15 00:21:06 +0530

    19

--- Roboshop-Project/components/frontend.sh
+++ Roboshop-Project/components/frontend.sh
@@ -6,7 +6,7 @@ source ./components/common.sh
 
 INFO "Setup frontend started"
 INFO "Installing Nginx"
-yum install nginx -y &>> "$LOG_OUT"
+yum install nginx -y
 
 SUCC "Installed Frontend"
 FAIL "Installing Frontend failed"
